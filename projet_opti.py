30e58e36e016abb304d86f379d2c35d7421b46f9
Gabrielle Berrada <gabrielleblj@gmail.com>
2022-05-03 17:42:50 +0200

    modifs

--- projet_opti.py
+++ projet_opti.py
@@ -1,6 +1,7 @@
 from casadi import *
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Question 4
 
@@ -31,25 +32,40 @@ def h(x1, x2, alpha = ALPHA):
     return (x1*np.exp(-alpha*x1) + x2*np.exp(-alpha*x2))/(np.exp(-alpha*x1)+ np.exp(-alpha*x2))
 
 opti = casadi.Opti()
-n = 2*(N+1)
-x = opti.variable(n)
+P = opti.variable(N+1)
+T = opti.variable(N+1)
 f = 0
 for i in range(1, N+1):
-    f -= h(E[i], x[i]*delta_t)
+    f -= h(E[i], P[i]*delta_t)
 opti.minimize(f)
-opti.subject_to(x[N] == 0)
-opti.subject_to(x[N+1]-T_in == 0)
+opti.subject_to(P[N] == 0)
+opti.subject_to(T[0]-T_in == 0)
 for i in range(1, N+1):
-    opti.subject_to(x[N+1+i]-np.exp(-k*delta_t)*x[N+1+i-1]-(1-np.exp(-k*delta_t))/k*C*(x[i-1]-Q[i-1])==0)
+    opti.subject_to(T[i]-np.exp(-k*delta_t)*T[i-1]-(1-np.exp(-k*delta_t))/k*C*(P[i-1]-Q[i-1])==0)
 for i in range(N+1):
-    opti.subject_to(-x[N+1+i]+273.15 <= 0)
-    opti.subject_to(x[N+1+i]-T_sat <= 0)
-    opti.subject_to(-x[i] <= 0)
-    opti.subject_to(x[i] - P_M <= 0)
+    opti.subject_to(-T[i]+273.15 <= 0)
+    opti.subject_to(T[i]-T_sat <= 0)
+    opti.subject_to(-P[i] <= 0)
+    opti.subject_to(P[i] - P_M <= 0)
 opti.solver('ipopt');
 sol = opti.solve();
-print(f' Pression =  {sol.value(x[N+1:])}')
-print( f' Température = {sol.value(x[:N+1])}')
+puissance1 = sol.value(P)
+temperature1 = sol.value(T) - 273.15
+print(f' Puissance (en W) =  {puissance1}')
+print( f' Température (en °C) = {temperature1}')
+
+###
+
+t = np.array([k*delta_t for k in range(N+1)])
+plt.plot(t, puissance1, label = 'Puissance')
+plt.plot(t, E, label = 'Energie produite')
+plt.legend()
+plt.show()
+
+
+plt.plot(t, temperature1, label = 'Temperature')
+plt.show()
+
 
 ## Question 10
 
@@ -58,10 +74,10 @@ opti = casadi.Opti()
 n = 2*(N+1)
 ns = N+1
 x = opti.variable(n)
-s = opti.variable(ns)
+s = opti.variable(N+1)
 f = 0
 for i in range(1, N+1):
-    f -= h(E[i], x[i]*delta_t)
+    f -= s[i]
 opti.minimize(f)
 opti.subject_to(x[N] == 0)
 opti.subject_to(x[N+1]-T_in == 0)
@@ -77,7 +93,9 @@ for i in range(ns):
     opti.subject_to(s[i]-E[i] <= 0)
 opti.solver('ipopt');
 sol = opti.solve();
-print(f' Pression =  {sol.value(x[N+1:])}')
-print( f' Température = {sol.value(x[:N+1])}')
+puissance2 = sol.value(x[:N+1])
+temperature2 = sol.value(x[N+1:])-273.15
+print(f' Puissance (en W) =  {puissance2}')
+print(f' Température (en °C) = {temperature2}')
 
 
